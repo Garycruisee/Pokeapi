d12587998451b5b538ff94cb43bd3152ce7f9620
Gary Cruise <Garycruisee@gmail.com>
2023-09-15 15:59:44 +0700

    feat: get pokeapi data and use useEffect to execute it - async await pokeapi - useEffect pokeapi - consolelog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h1>Hello</h1>;
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  const getAllPokemon = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1>Pokemon</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+        <button className="load-more">Load more</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
